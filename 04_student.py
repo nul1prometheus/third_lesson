dd1637e353751314cd515eb0bd6b5d3b3c0214ea
Tim <timofey250195@icloud.com>
2022-01-24 14:28:28 +0300

    fourth done

--- 04_student.py
+++ 04_student.py
@@ -8,7 +8,14 @@
 # чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
-
+x = 0
 educational_grant, expenses = 10000, 12000
+parental_money = expenses - educational_grant
+while x <= 10:
+    x = x + 1
+    expenses = expenses * 1.03
+    expenses = float('{:.2f}'.format(expenses))
+    print(expenses)
+    parental_money = parental_money + expenses - educational_grant
 
-# TODO здесь ваш код
+print('From parents to year', parental_money)
